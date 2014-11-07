edb278819b6a024e79f31b3e0dcf655f32444936
Jin Jay <jinjaysnow@gmail.com>
2014-11-07 10:58:59 +0800

    my modify

--- Src/cssHandle.h
+++ Src/cssHandle.h
@@ -19,17 +19,17 @@ enum NodeType {
     elementNode = 3,
 };
 typedef struct node {
-    int    type;
-    char   name[20];
+    enum NodeType    type;
+    char   name[30];
     struct node* next;
 }node;
 typedef struct nodeList {
-    node* head;             //head节点无数据，只有next
+    node* head;             
     int   type;
-    int   priority;         //优先级
+    int   priority;  
 }nodeList;
 typedef struct cssNode {
-    struct nodeList* nodes; //选择器选择的结点链
+    struct nodeList* nodes; 
     enum cssType type;
     char display[10];
     char position[10];
@@ -50,43 +50,8 @@ typedef struct cssNode {
     char fontWeight[10];
     char lineBreak[10];
     struct cssNode *next;
-}cssNode;
-typedef struct cssList
-{
-    cssNode* head;          //head节点无数据，只有next
-}cssList;
-typedef struct rule
-{
-    char name[15];
-    int namePos;
-    char value[30];
-    int valPos;
-    struct rule* next;
-}rule;
-typedef struct ruleList {
-    rule* head;             //head节点无数据，只有next
-    int type;
-}ruleList;
-static char attributes[18][15]= {
-    "display",      //1
-    "position",     //2
-    "width",        //3
-    "height",       //4
-    "top",          //5
-    "bottom",       //6
-    "left",         //7
-    "right",        //8
-    "padding",      //9
-    "border",       //10
-    "margin",       //11
-    "color",        //12
-    "font-size",    //13
-    "line-height",  //14
-    "text-align",   //15
-    "font-style",   //16
-    "font-weight",  //17
-    "line-break",   //18
-};
+}cssList, cssNode;
+
 extern cssList* handleCss(const char* buffer);
 extern void freeCssList(cssList* csss);
 
